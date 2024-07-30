c4f35b1d3d140f0270886d42697f4ecbe9875f08
Vaibhav <Vaibhav@DESKTOP-4MR0K6C.attlocal.net>
2024-07-29 22:51:14 -0700

    The contentPane has a working background with JPanel squares.

--- src/chessFinal/Display.java
+++ src/chessFinal/Display.java
@@ -1,20 +1,29 @@
 package chessFinal;
 
+import java.awt.GridLayout;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 public class Display extends JFrame {
 
 	private static final double ASPECT_RATIO = 1.0; // Keeps the window squared
+	private JPanel contentPane;
 
 	public Display() {
-
-		initializeBackground();
+		
+		// Sets up JFrame features
 		setTitle("Chess");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setupSizing();
+		// Setup content pane for viewing items
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(8, 8, 0, 0)); // Chess layout
+		initializeBackground();
 
 	}
 
@@ -37,9 +46,15 @@ public class Display extends JFrame {
 
 	void initializeBackground() {
 		
+		int number = 0; // Used to have alternating colors
 		for (int i = 0; i < 8; i++) {
 			for (int j = 0; j < 8; j++) {
-				
+				// Tinkered w/ line under to get correct color order
+				boolean defaultColor = ((i + number) % 2 == 1) ? false : true; 
+				Square square = new Square(defaultColor, i, j);
+				number++;
+				// Add square to our JFrame
+				contentPane.add(square, i, j);
 			}
 		}
 		
--- src/chessFinal/Square.java
+++ src/chessFinal/Square.java
@@ -1,13 +1,22 @@
 package chessFinal;
 
+import java.awt.Color;
+
 import javax.swing.JPanel;
 
 public class Square extends JPanel {
 	
-	boolean side; // black is 0, white is 1
-	int position;
+	boolean defaultColor; // black is 0, white is 1
+	int posX, posY;
 	
-	public Square(boolean side) {
+	public Square(boolean defaultColor, int posX, int posY) {
+		
+		this.defaultColor = defaultColor;
+		this.posX = posX;
+		this.posY = posY;
+		
+		if (defaultColor) setBackground(Color.DARK_GRAY);
+		else setBackground(Color.WHITE);
 		
 	}
 	
