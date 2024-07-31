7c32394a187806bfc3221637c88070a026aca247
Vaibhav <Vaibhav@DESKTOP-4MR0K6C.attlocal.net>
2024-07-31 01:10:48 -0700

    Has dragging pieces feature implemented, but piece cant leave JPanel.

--- src/chessFinal/Square.java
+++ src/chessFinal/Square.java
@@ -2,6 +2,10 @@ package chessFinal;
 
 import java.awt.Color;
 import java.awt.Image;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -35,13 +39,14 @@ public class Square extends JPanel {
 			setBackground(Color.WHITE);
 		setPiecePictures(); // Prevent nullptr
 		add(img); // Add JLabel image for pieces
+		handleInput();
 	}
 
-	void setPiece(Piece piece) {
+	public void setPiece(Piece piece) {
 		this.piece = piece;
 	}
 
-	void repaintSquare() {
+	public void repaintSquare() {
 
 		int num = piece != null ? piece.type : -1;
 
@@ -118,4 +123,67 @@ public class Square extends JPanel {
 		}
 	}
 
+	private void handleInput() {
+		addMouseListener(new MouseListener() {
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				startImageFollowing(e);
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+				stopImageFollowing();
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+			}
+			
+		});
+		
+		addMouseMotionListener(new MouseMotionListener() {
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				moveImageWithCursor(e);
+			}
+
+			@Override
+			public void mouseMoved(MouseEvent e) {}
+		});
+	}
+	
+	// Method to start following the cursor with the image
+		private void startImageFollowing(MouseEvent e) {
+			img.setVisible(true); // Make the image visible if it's hidden
+			moveImageWithCursor(e); // Start by moving the image to the cursor position
+		}
+
+		// Method to stop following the cursor with the image
+		private void stopImageFollowing() {
+			img.setVisible(false); // Hide the image when the mouse is released
+		}
+
+		// Method to move the image with the cursor
+		private void moveImageWithCursor(MouseEvent e) {
+			Point p = e.getPoint();
+			img.setLocation(p.x - img.getWidth() / 2, p.y - img.getHeight() / 2);
+			repaint(); // Repaint to update the position
+		}
 }
+
+
